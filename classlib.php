d209dfaed285898c3b6a6c3821ea1ed8833e5912
Patrick Berenschot <patrick.stroef@gmail.com>
2012-10-03 22:15:29 +0200

    getStatic beveiligd en saveQuery function toegevoegd

--- classlib.php
+++ classlib.php
@@ -2,52 +2,69 @@
 
 class SQLConnect {
 	var $usr;
-    var $pwd;
+	var $pwd;
 	var $db;
-    var $host;
+	var $host;
 	var $mysqli;
-	
-	function __construct(){
+
+	function __construct()
+	{
 		$this->usr = "";
 		$this->pwd = "";
 		$this->db = "";
 		$this->host = "localhost";
 		$this->mysqli = new mysqli($this->host, $this->usr, $this->pwd, $this->db);
 	}
-	
-	function escapeInput($_in){
-		return $this->mysqli->real_escape_string($_in);	
+
+	function escapeInput($_in)
+	{
+		return $this->mysqli->real_escape_string($_in);
 	}
-	
-	function queryDB($_q){
+
+	function queryDB($_q)
+	{
 		$result = $this->mysqli->query($_q);
 		$row = $result->fetch_assoc();
-		return $row;	
+		return $row;
 	}
+	
+	function saveQuery($_q){
+		$this->queryDB($this->escapeInput($_q));
+	}
+
 }
 
 class PageHandle {
-	var $dbConnect;//voor het escapen van input
+	var $dbConnect;
+	//voor het escapen van input
 
-	function __construct(){
-			$this->dbConnect = new SQLConnect();
+	function __construct()
+	{
+		$this->dbConnect = new SQLConnect();
 	}
-	
-	function handleGET(){
-		
-		if(is_numeric($_GET[id])){
+
+	function handleGET()
+	{
+
+		if(is_numeric($_GET[id]))
+		{
 			//nummer opgegeven in GET
 			$clean_ID = $this->dbConnect->escapeInput($_GET['id']);
 			//numeric zou al voldoende moeten zijn, maar ALTIJD input scrubben
-			return $clean_ID;		
-		}else if(!$_GET){
+			return $clean_ID;
+		}
+		else if( ! $_GET)
+		{
 			//geen GET data -> geland op de site
-			return 1;					
-		}else {
+			return 1;
+		}
+		else
+		{
 			//ongeldige GET data
 			return 1;
 		}
 	}
+
 }
 
 class Page {
@@ -55,22 +72,25 @@ class Page {
 	var $title;
 	var $html;
 	var $dbConnect;
-		
-	function __construct($_id){
+
+	function __construct($_id)
+	{
 		$this->id = $_id;
 		$this->dbConnect = new SQLConnect();
 	}
-	
+
 	//method om content uit de DB te halen
-	function getContent($_name){
-		$row = $this->dbConnect->queryDB("SELECT ".$_name." FROM cms_artikelen WHERE id = ".$this->id."");
+	function getContent($_name)
+	{
+		$row = $this->dbConnect->saveQuery("SELECT " . $_name . " FROM cms_artikelen WHERE id = " . $this->id . "");
 		return $row[$_name];
 	}
-		
+
 	//method om statische blokken op te halen
-	function getStatic($_name){
-		$_name .= ".php";
-		include $_name;
+	function getStatic($_name)
+	{
+		$_name = basename($_name);
+		include 'static/' . $_name . ".php";
 	}
 
 }
