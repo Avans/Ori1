92187517b95287310a783520c52ef02a300dc24f
Patrick Berenschot <patrick.stroef@gmail.com>
2012-10-04 12:29:16 +0200

    Zo, frontend er weer in gedaan

--- index.php
+++ index.php
@@ -7,32 +7,8 @@
 	$id = $handle->handleGET('id');
 	if($id > 0)$curPage->setID($id);
 	
+
+echo $curPage->getStatic('header');
+echo $curPage->getContent('html');
+echo $curPage->getStatic('footer');
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>CMS test</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" type="text/css" href="static/css/style_cms.css">
-		
-	</head>
-	<body>
-		<div class="centerPage">
-			<?= $curPage->getStatic('header')?>
-			<div class="page_container">
-				<div class="page_heading">
-					<h3><?= $curPage->getContent('title')?></h3>
-				</div>
-				
-				<div class="page_content">
-					<?= $curPage->getContent('html')?>
-				</div>
-				<div class="page_sidebar">
-					<?= $curPage->getContent('html_side')?>
-				</div>
-			
-			</div>
-			<?= $curPage->getStatic('footer')?>
-		</div>
-	</body>
-</html>	
--- static/footer.php
+++ static/footer.php
@@ -1,1 +1,17 @@
-<h3>footer</h3>
+    </div>
+    <aside>
+        <div class="stereotype">
+            Stereotype 1
+        </div>
+        <div class="stereotype">
+            Stereotype 2
+        </div>
+        <div class="stereotype">
+            Stereotype 3
+        </div>
+        <div class="stereotype">
+            Stereotype 4
+        </div>
+    </aside>
+</body>
+</html>
