27cd38ee16c7afa539f476e3a8e2d5d04958f75c
Patrick Berenschot <patrick.stroef@gmail.com>
2012-10-10 12:09:34 +0200

    Uiterlijk afgerond en afbeeldingen bijgevoegd

--- static/footer.php
+++ static/footer.php
@@ -1,17 +1,9 @@
-    </div>
+</div>
     <aside>
-        <div class="stereotype">
-            Stereotype 1
-        </div>
-        <div class="stereotype">
-            Stereotype 2
-        </div>
-        <div class="stereotype">
-            Stereotype 3
-        </div>
-        <div class="stereotype">
-            Stereotype 4
-        </div>
+        <a href="#"><img src="static/img/koffie.jpg" alt="ICT-ers drinken veel Cafe&iuml;ne" title="ICT-ers drinken veel Cafe&iuml;ne" width="200" height="150" /></a>
+        <a href="#"><img src="static/img/nerd.jpg" alt="ICT-ers dragen nerd kleren" title="ICT-ers dragen nerd kleren" width="200" height="150"/></a>
+        <a href="#"><img src="static/img/starwars.jpg" alt="ICT-ers zijn geeky" title="ICT-ers zijn geeky" width="200" height="150"/></a>
+        <a href="#"><img src="static/img/vragenlijst.jpg" alt="Maak de vragen lijst!" title="Maak de vragen lijst!" width="200" height="150"/></a>
     </aside>
 </body>
 </html>
--- static/header.php
+++ static/header.php
@@ -4,77 +4,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php echo $curPage->getContent('title'); ?></title>
     <meta name="description" content="">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <style type="text/css">
-		html, body {
-			height: 100%;
-		}
-
-		html {
-			background-color: #2E282D;
-		}
-
-		body {
-			width: 1000px;
-			margin: 0 auto;
-			background-color: #F6F6F6;
-		}
-
-		aside {
-			padding-top: 14px;
-			float: left;
-			display: block;
-			width: 20%;
-		}
-
-		#main {
-			padding: 25px;
-			float: left;
-			width: 70%;
-		}
-		header {
-			/* het logo */
-			background-image: url(http://creativecommons.org/images/deed/cc-logo.jpg);
-			background-repeat: no-repeat;
-			padding-left: 150px;
-			height: 150px;
-		}
-
-		a:link {
-			color: #CEE864;
-		}
-		a:visited {
-			color: #717D61;
-		}
-		a:hover {
-			color: #F74114;
-		}
-
-		li, ul {
-			display: inline;
-			margin: 0px;
-			padding: 2px;
-		}
-
-		ul {
-			margin: 50px;
-			border: 1px solid black;
-		}
-
-		#carousel {
-			border: 1px solid black;
-			width: 650px;
-			height: 200px;
-		}
-
-		.stereotype {
-			border: 1px solid black;
-			width: 100%;
-			height: 150px;
-			margin-top: 10px;
-		}
-    </style>
+    <link rel="stylesheet" href="../static/css/normalize.css">
+    <link rel="stylesheet" href="../static/css/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
