dd8033f074d9178c0dc31cf288f467a3623ea9a7
Patrick Berenschot <patrick.stroef@gmail.com>
2012-10-11 14:31:09 +0200

    Bar kleuren verwijderd, houd het aantal ingevulden vragen bij en geef het resultaat weer.

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
-	error_reporting(0);
-	require_once 'classlib.php';
-	$handle = new PageHandle();
-	$curPage = new Page();	
-	$id = 7;
-	if($id > 0)$curPage->setID($id);
-	
+error_reporting(0);
+require_once 'classlib.php';
+$handle = new PageHandle();
+$curPage = new Page();
+$id = 7;
+if ($id > 0)
+	$curPage -> setID($id);
 
 //echo $curPage->getStatic('header');
 ?>
@@ -13,7 +13,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php echo $curPage->getContent('title'); ?></title>
+    <title><?php echo $curPage -> getContent('title'); ?></title>
     <meta name="description" content="">
     <link rel="stylesheet" href="static/css/normalize.css">
     <link rel="stylesheet" href="static/css/main.css"><script src="http://code.jquery.com/jquery-latest.js"></script>
@@ -29,7 +29,7 @@
     </header>
 
     <div id="main">
-        <?php echo $curPage->getContent('html'); ?>
+        <?php echo $curPage -> getContent('html'); ?>
     </div>
 
     <aside>
@@ -41,67 +41,73 @@
 
 </body>
 <script>
-    var peter = 0;
-    var hans = 0;
-    var lucy = 0;
+	var peter = 0;
+	var hans = 0;
+	var lucy = 0;
 
-    function updatePersona(realname) {
+	var answerdQuestions = 0;
+	var numQuestions = $('.question').length
 
-        //Get the variable by the name realname
-        window[realname]++;
-        var element_id = "#" + realname + "_bar";
+	function updatePersona(realname) {
 
-        //Update Progressbar, nine points per person
-        $(element_id).progressbar("value", window[realname] * 100 / 9);
-    }
+		//Get the variable by the name realname
+		window[realname]++;
+		var element_id = "#" + realname + "_bar";
+
+		//Number of posibilities per person
+		var amountPossible = 12;
+		//Update Progressbar
+		$(element_id).progressbar("value", window[realname] * 100 / amountPossible);
+	}
+
+	function showPersona() {
+		var array = new Array;
+		var nr = Math.max(peter, hans, lucy);
+		if (nr == lucy) {
+			array.push("lucy");
+		}
+
+		if (nr == hans) {
+			array.push("hans");
+		}
+
+		if (nr == peter) {
+			array.push("peter");
+		}
+
+		$('#main').empty();
+		$('#main').append('<h1>Jij bent een ');
+		for (var i = 0; i < array.length; i++) {
+			$('#main').append(array[i] + ' ');
+		}
+		$('#main').append('</h1>');
+	}
 
 
-    $("button").click(function() {
+	$("button").click(function() {
 
-        /*
-         //Get all the siblings
-         var $siblings = $(this).parent().children();
-         alert($siblings);
-         //Slide the question away
-         $siblings.slideUp();
-         $siblings.parent().slideUp();
-         */
-        $(this).parent('p').slideUp();
-        var classes = $(this).attr('class');
-        var classes_arr = classes.split(" ");
+		answerdQuestions++;
 
-        $.each(classes_arr, function(index, value) {
-            updatePersona(value);
-        });
-        //Prevent button from firing
-        return false;
-    });
-    //Init the progress bars
-    $("#peter_bar, #hans_bar, #lucy_bar").progressbar({
-        value : 0
+		$(this).parent('p').slideUp();
+		var classes = $(this).attr('class');
+		var classes_arr = classes.split(" ");
 
-        //Update the colors on value change
-    }).bind('progressbarchange', function(event, ui) {
-        var selector = "#" + this.id + " > div";
-        var value = this.getAttribute("aria-valuenow");
-        if(value < 10) {
-            $(selector).css({
-                'background' : 'Red'
-            });
-        } else if(value < 30) {
-            $(selector).css({
-                'background' : 'Orange'
-            });
-        } else if(value < 50) {
-            $(selector).css({
-                'background' : 'Yellow'
-            });
-        } else {
-            $(selector).css({
-                'background' : 'Green'
-            });
-        }
-    });
+		$.each(classes_arr, function(index, value) {
+			updatePersona(value);
+		});
+
+		if (answerdQuestions == numQuestions) {
+			showPersona();
+		}
+
+		//Prevent button from firing
+		return false;
+	});
+	
+	//Init the progress bars
+	$("#peter_bar, #hans_bar, #lucy_bar").progressbar({
+		value : 0
+	});
 
 </script>
 </html>
