05f7a429c006b78b65590edb0ef9be8292273af0
Patrick Berenschot <patrick.stroef@gmail.com>
2012-10-11 13:59:54 +0200

    Links werkend gemaakt

--- static/footer.php
+++ static/footer.php
@@ -1,9 +1,9 @@
 </div>
     <aside>
-        <a href="#"><img src="static/img/peter.jpg" alt="Peter" title="Peter" width="200" height="150" /></a>
-        <a href="#"><img src="static/img/lucy.jpg" alt="Lucy" title="Lucy" width="200" height="150"/></a>
-        <a href="#"><img src="static/img/hans.jpg" alt="Hans" title="Hans" width="200" height="150"/></a>
-        <a href="#"><img src="static/img/vragenlijst.jpg" alt="Maak de vragen lijst!" title="Maak de vragen lijst!" width="200" height="150"/></a>
+        <a href="index.php?id=4"><img src="static/img/peter.jpg" alt="Peter" title="Peter" width="200" height="150" /></a>
+        <a href="index.php?id=5"><img src="static/img/lucy.jpg" alt="Lucy" title="Lucy" width="200" height="150"/></a>
+        <a href="index.php?id=6"><img src="static/img/hans.jpg" alt="Hans" title="Hans" width="200" height="150"/></a>
+        <a href="test.php"><img src="static/img/vragenlijst.jpg" alt="Maak de vragen lijst!" title="Maak de vragen lijst!" width="200" height="150"/></a>
     </aside>
 </body>
 </html>
